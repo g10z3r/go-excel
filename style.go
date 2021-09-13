0ce0a069a32309f435ee7d6c226d668f8ed0c4ce
I0HuKc <web-disain@mail.ua>
2021-09-13 13:02:53 +0300

    Добавил новых стилей

--- style.go
+++ style.go
@@ -1,5 +1,7 @@
 package excel
 
+import "fmt"
+
 // Стили для шапки таблиц
 const (
 	GreenHeadStyle string = `{
@@ -11,7 +13,29 @@ const (
 		"alignment": {"horizontal":"center", "vertical":"center", "wrap_text":true},
 		"fill":{"type":"pattern","color":["#9BB958"],"pattern":1}
 	}`
+
+	BlueHeadStyle string = `{
+		"border":[
+			{"type":"left","color":"ffffff","style":1},	
+			{"type":"right","color":"ffffff","style":1}
+		],		
+		"font": {"bold":true,"size":11, "color":"FFFFFF"},
+		"alignment": {"horizontal":"center", "vertical":"center", "wrap_text":true},
+		"fill":{"type":"pattern","color":["#4F81BD"],"pattern":1}
+	}`
 )
+
+func CustomHeadStyle(fontColor, fillColor string) string {
+	return fmt.Sprintf(`{
+		"border":[
+			{"type":"left","color":"ffffff","style":1},	
+			{"type":"right","color":"ffffff","style":1}
+		],		
+		"font": {"bold":true,"size":11, "color":"%s"},
+		"alignment": {"horizontal":"center", "vertical":"center", "wrap_text":true},
+		"fill":{"type":"pattern","color":["%s"],"pattern":1}
+	}`, fontColor, fillColor)
+}
 
 // Варианты стилей для рядов
 const (
@@ -36,4 +60,40 @@ const (
 		"alignment": {"horizontal":"center", "vertical":"center", "wrap_text":true},
 		"fill":{"type":"pattern","color":["#ECF0E1"],"pattern":1}
 	}`
+
+	WhiteRowStyle string = `{
+		"border":[
+			{"type":"top","color":"ffffff","style":1},	
+			{"type":"bottom","color":"ffffff","style":1},
+			{"type":"left","color":"ffffff","style":1},	
+			{"type":"right","color":"ffffff","style":1}
+		],	
+		"alignment": {"horizontal":"center", "vertical":"center", "wrap_text":true},
+		"fill":{"type":"pattern","color":["#FFFFFF"],"pattern":1}
+	}`
+
+	LightBlueRowStyle string = `{
+		"border":[
+			{"type":"top","color":"ffffff","style":1},	
+			{"type":"bottom","color":"ffffff","style":1},
+			{"type":"left","color":"ffffff","style":1},	
+			{"type":"right","color":"ffffff","style":1}
+		],
+		"alignment": {"horizontal":"center", "vertical":"center", "wrap_text":true},
+		"fill":{"type":"pattern","color":["#DCE5F0"],"pattern":1}
+	}`
 )
+
+func CustomRowStyle(fontColor, fillColor string) string {
+	return fmt.Sprintf(`{
+		"border":[
+			{"type":"top","color":"ffffff","style":1},	
+			{"type":"bottom","color":"ffffff","style":1},
+			{"type":"left","color":"ffffff","style":1},	
+			{"type":"right","color":"ffffff","style":1}
+		],
+		"font": {"bold":true,"size":11, "color":"%s"},
+		"alignment": {"horizontal":"center", "vertical":"center", "wrap_text":true},
+		"fill":{"type":"pattern","color":["%s"],"pattern":1}
+	}`, fontColor, fillColor)
+}
